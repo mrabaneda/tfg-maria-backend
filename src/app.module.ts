01a4ba9e114dea798e595044aeba588f520e058c
mrabaneda <mrabaneda18@gmail.com>
2024-10-04 14:26:25 +0200

    feat: add user controller module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@
 
 import { Module } from '@nestjs/common';
 import { SetUpModule } from './setup.module';
+import UserControllerModule from './adapters/infrastructure/di/controllers/user_controller.module';
 import AdminUserControllerModule from './adapters/infrastructure/di/controllers/admin_user_controller.module';
 
 // --------------------------------
@@ -11,7 +12,7 @@ import AdminUserControllerModule from './adapters/infrastructure/di/controllers/
 // --------------------------------
 
 @Module({
-  imports: [SetUpModule, AdminUserControllerModule],
+  imports: [SetUpModule, AdminUserControllerModule, UserControllerModule],
 })
 class AppModule {}
 
