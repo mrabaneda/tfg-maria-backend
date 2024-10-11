9dfee05651185bd8a1601eec41c14e368ea9c712
mrabaneda <mrabaneda18@gmail.com>
2024-10-11 12:54:38 +0200

    feat: add white list (cors policy)

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import { NestFactory } from '@nestjs/core';
 (async () => {
   const nestApplication = await NestFactory.create(AppModule);
 
-  nestApplication.enableCors({ methods: '*', origin: '*' });
+  nestApplication.enableCors({ methods: '*', credentials: true, origin: process.env.WHITE_LIST!.split(';') });
 
   await nestApplication.listen(Number(process.env.PORT!));
 })();
