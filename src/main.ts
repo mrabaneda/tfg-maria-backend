6f1b67a4b69080ef356cfe6b326ba3fc7c39c85a
mrabaneda <mrabaneda18@gmail.com>
2024-10-15 01:03:23 +0200

    build: specific CORS policy (unallowed delete with *)

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,11 @@ import { NestFactory } from '@nestjs/core';
 (async () => {
   const nestApplication = await NestFactory.create(AppModule);
 
-  nestApplication.enableCors({ methods: '*', credentials: true, origin: process.env.WHITE_LIST!.split(';') });
+  nestApplication.enableCors({
+    methods: ['GET', 'POST', 'DELETE'],
+    credentials: true,
+    origin: process.env.WHITE_LIST!.split(';'),
+  });
 
   await nestApplication.listen(Number(process.env.PORT!));
 })();
